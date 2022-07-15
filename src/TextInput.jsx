1c7026cf8c5255676844bfcb9816fb6bee7777b4
Nitika Agarwal <nitika.agarwal@miraash.com>
2022-07-15 21:49:50 +0530

    add: model

--- src/TextInput.jsx
+++ src/TextInput.jsx
@@ -1,73 +1,98 @@
-import {useState} from 'react'
-import './App.css';
-import axios from 'axios';
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function TextInput() {
-  const [tone1, setTone1] = useState('Converted Text')
-  const [tone2, setTone2] = useState('Converted Text')
-  const [tone3, setTone3] = useState('Converted Text')
-//   const [loader, setLoader] = useState(false)
+  const [tone1, setTone1] = useState("Converted Text");
+  const [tone2, setTone2] = useState("Converted Text");
+  const [tone3, setTone3] = useState("Converted Text");
+  const [heading, setHeading] = useState("Output");
+  //   const [loader, setLoader] = useState(false)
 
-  const handleKeyPress = (event) =>{
-    console.log(event.target.value, event)
+  const handleKeyPress = (event) => {
+    console.log(event.target.value, event);
     // setLoader(true)
-    if(event.ctrlKey){
-        console.log(event.keyCode)
+    if (event.ctrlKey) {
+      console.log(event.keyCode);
       let model = "";
-      if(event.keyCode ===  49) {
-        model = "old"
-        axios.post('http://34.172.89.50:8000/get_suggestions', {
-      text: event.target.value.trim().length ? event.target.value : '<BOS>',
-      model:model,
-      max_length:'10'
-    }, {
-      headers:{  "Access-Control-Allow-Origin": "*",  'Content-Type': 'application/json',}
-    })
-    .then(function (response) {
-    //   setLoader(false)
-      console.log(response);
-      setTone1(response?.data?.Suggestions[0])
-      setTone2(response?.data?.Suggestions[1])
-      setTone3(response?.data?.Suggestions[2])
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+      if (event.keyCode === 49) {
+        model = "old";
+        setHeading("Output: old");
+        axios
+          .post(
+            "http://34.172.89.50:8000/get_suggestions",
+            {
+              text: event.target.value.trim().length
+                ? event.target.value
+                : "<BOS>",
+              model: model,
+              max_length: "10",
+            },
+            {
+              headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json",
+              },
+            }
+          )
+          .then(function (response) {
+            //   setLoader(false)
+            console.log(response);
+            setTone1(response?.data?.Suggestions[0]);
+            setTone2(response?.data?.Suggestions[1]);
+            setTone3(response?.data?.Suggestions[2]);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
       } else if (event.keyCode === 48) {
-        model ='new';
-        axios.post('http://34.172.89.50:8000/get_suggestions', {
-      text: event.target.value.trim().length ? event.target.value : '<BOS>',
-      model:model,
-      max_length:'10'
-    }, {
-      headers:{  "Access-Control-Allow-Origin": "*",  'Content-Type': 'application/json',}
-    })
-    .then(function (response) {
-    //   setLoader(false)
-      console.log(response);
-      setTone1(response?.data?.Suggestions[0])
-      setTone2(response?.data?.Suggestions[1])
-      setTone3(response?.data?.Suggestions[2])
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+        model = "new";
+        setHeading("Output: new");
+        axios
+          .post(
+            "http://34.172.89.50:8000/get_suggestions",
+            {
+              text: event.target.value.trim().length
+                ? event.target.value
+                : "<BOS>",
+              model: model,
+              max_length: "10",
+            },
+            {
+              headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json",
+              },
+            }
+          )
+          .then(function (response) {
+            //   setLoader(false)
+            console.log(response);
+            setTone1(response?.data?.Suggestions[0]);
+            setTone2(response?.data?.Suggestions[1]);
+            setTone3(response?.data?.Suggestions[2]);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
       }
-    
     }
-  }
+  };
   return (
-  
     <div className="flex h-screen w-screen items-center justify-center">
-    <div className="flex gap-[5px]">
-      <div className="inline-flex flex-col mr-[85px]">
-        <h1 className="text-4xl font-bold mb-[10px]">Input</h1>
-        <textarea placeholder="Press Ctrl + 0 for new model. Press Ctrl + 1 for old model" className="border border-solid border-blue w-[500px] h-[250px] outline-none bg-blue-light text-grey p-[15px] text-xl font-medium rounded-[4px]" style={{resize:'none'}} name="text" onKeyDown={(e) => {
-          handleKeyPress(e) 
-        }}>
-        
-        </textarea>
-        {/* <div className="inline-flex justify-end mt-[20px] gap-[10px]">
+      <div className="flex gap-[5px]">
+        <div className="inline-flex flex-col mr-[85px]">
+          <h1 className="text-4xl font-bold mb-[10px]">Input</h1>
+          <textarea
+            placeholder="Press Ctrl + 0 for new model. Press Ctrl + 1 for old model"
+            className="border border-solid border-blue w-[500px] h-[250px] outline-none bg-blue-light text-grey p-[15px] text-xl font-medium rounded-[4px]"
+            style={{ resize: "none" }}
+            name="text"
+            onKeyDown={(e) => {
+              handleKeyPress(e);
+            }}
+          ></textarea>
+          {/* <div className="inline-flex justify-end mt-[20px] gap-[10px]">
         <div className="cursor-pointer bg-blue text-white w-[147px] h-[36px] items-center justify-center inline-flex text-xl rounded-[4px]" onClick={() => {
         setTone1('People Are Not Happy About Changes To The Starbucks Rewards Program')
       }}>Normal</div>
@@ -75,25 +100,25 @@ function TextInput() {
         setTone1('People Are Not Happy About Changes To The Starbucks Rewards Program')
       }}>Buzz</div>
         </div> */}
-      </div>
-      <div className="inline-flex flex-col justify-start max-w-[350px] w-[350px]">
-      <h1 className="text-4xl font-bold mb-[10px]">Output</h1>
-      <div className="mb-[40px]">
-      <div className="text-xl text-blue font-medium">Suggestion 1</div>
-      <div className="text-xl text-grey  font-medium">{tone1}</div>
-      </div>
-      <div className="mb-[40px]">
-      <div className="text-xl text-blue font-medium">Suggestion 2</div>
-      <div className="text-xl text-grey  font-medium">{tone2}</div>
-      </div>
-      <div className="mb-[40px]">
-      <div className="text-xl text-blue font-medium">Suggestion 3</div>
-      <div className="text-xl text-grey  font-medium">{tone3}</div>
-      </div>
-      </div>
+        </div>
+        <div className="inline-flex flex-col justify-start max-w-[350px] w-[350px]">
+          <h1 className="text-4xl font-bold mb-[10px]">{heading}</h1>
+          <div className="mb-[40px]">
+            <div className="text-xl text-blue font-medium">Suggestion 1</div>
+            <div className="text-xl text-grey  font-medium">{tone1}</div>
+          </div>
+          <div className="mb-[40px]">
+            <div className="text-xl text-blue font-medium">Suggestion 2</div>
+            <div className="text-xl text-grey  font-medium">{tone2}</div>
+          </div>
+          <div className="mb-[40px]">
+            <div className="text-xl text-blue font-medium">Suggestion 3</div>
+            <div className="text-xl text-grey  font-medium">{tone3}</div>
+          </div>
+        </div>
       </div>
     </div>
-  ); 
+  );
 }
 
 export default TextInput;
