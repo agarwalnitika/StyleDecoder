b6263b8cec1d2f488c4cbce87069981d6240da55
Nitika Agarwal <nitika.agarwal@miraash.com>
2022-07-15 21:22:23 +0530

    decoder

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-
-import { useState } from 'react';
-import { Form } from './components/Form';
-import './styles.css'
-import TextInput from './TextInput';
+import { useState } from "react";
+import { Form } from "./components/Form";
+import "./styles.css";
+import TextInput from "./TextInput";
 // import TextInput from './TextInput';
 
 function App() {
-  const [step, setStep] = useState(1)
- return (
-   <>
-  <button type="submit" onClick={() => {
+  const [step, setStep] = useState(1);
+  return (
+    <>
+      {/* <button type="submit" onClick={() => {
     if(step === 1) {setStep(2)} else {setStep(1)}
   }}>{step ===2 ? 'Text Decoder' : 'Step Form'}</button>
-    {step === 1 ? <TextInput/> :<Form/>}
-   </>
-  ); 
+    {step === 1 ? <TextInput/> :<Form/>} */}
+      <TextInput />
+    </>
+  );
 }
 
 export default App;
-
