34e6f4530abbeb64c97f65610045fa04bc5f95b5
Nitika Agarwal <nitika.agarwal@miraash.com>
2022-07-15 19:37:56 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import TextInput from './TextInput';
+
+function App() {
+ return (
+   <>
+   <TextInput/>
+   </>
+  ); 
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'blue':'#5A87A1',
+        'blue-light':'#5A87A11A',
+        'grey':'#00000099'
+      }
+    },
+  },
+  plugins: [],
+}
